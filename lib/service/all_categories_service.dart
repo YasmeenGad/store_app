a6835647b4fe23532fa287528908de72355bbbab
yasmeen <yasmynjad53@gmail.com>
2023-10-26 05:41:35 +0300

    handle status code in three services

--- lib/service/all_categories_service.dart
+++ lib/service/all_categories_service.dart
@@ -5,7 +5,12 @@ class AllCategoriesService {
   Future<List<dynamic>> getAllCategories() async {
     http.Response response = await http
         .get(Uri.parse("https://fakestoreapi.com/products/categories"));
-    List<dynamic> data = jsonDecode(response.body);
-    return data;
+    if (response.statusCode == 200) {
+      List<dynamic> data = jsonDecode(response.body);
+      return data;
+    } else {
+      throw Exception(
+          'there is an exception in status code ${response.statusCode}');
+    }
   }
 }
--- lib/service/get_all_products.dart
+++ lib/service/get_all_products.dart
@@ -7,11 +7,16 @@ class AllProductsService {
   Future<List<ProductModel>> getAllProducts() async {
     http.Response response =
         await http.get(Uri.parse("https://fakestoreapi.com/products"));
-    List<dynamic> data = jsonDecode(response.body);
-    List<ProductModel> productsList = [];
-    for (int i = 0; i < data.length; i++) {
-      productsList.add(data[i]);
+    if (response.statusCode == 200) {
+      List<dynamic> data = jsonDecode(response.body);
+      List<ProductModel> productsList = [];
+      for (int i = 0; i < data.length; i++) {
+        productsList.add(data[i]);
+      }
+      return productsList;
+    } else {
+      throw Exception(
+          'there is an exception in status code ${response.statusCode}');
     }
-    return productsList;
   }
 }
