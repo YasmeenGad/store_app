378e5e18544f509ad045eee9ed8d3c24f3ca53d7
yasmeen <yasmynjad53@gmail.com>
2023-10-27 22:42:28 +0200

    create app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:store_app/screens/home.dart';
 
@@ -11,10 +12,17 @@ class StoreApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: Home.routeName,
-      getPages: [GetPage(name: Home.routeName, page: () => Home())],
+    return ScreenUtilInit(
+      designSize: Size(400, 820),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          initialRoute: Home.routeName,
+          getPages: [GetPage(name: Home.routeName, page: () => Home())],
+        );
+      },
     );
   }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
-  static const String routeName = "home";
+  static const String routeName = "/home";
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(FontAwesomeIcons.cartPlus, color: Colors.black))
+        ],
+        title: Text(
+          "New Trend",
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
   }
 }
